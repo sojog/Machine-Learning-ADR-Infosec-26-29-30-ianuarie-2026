9622de32cd63ca140485950a13dc4c023d240e90
Silviu Ojog <sil.ojog@gmail.com>
2026-01-26 10:36:21 +0200

    Sintaxa Python

--- 01.hello.py
+++ 01.hello.py
@@ -1,15 +1,30 @@
 
 print("Hello World!!!")
 
+
+
+
+print('Salutare Lume')
+
+print("""Salutare
+      
+      
+       Lume""")
+
+print('''Salutare 
+      
+      
+      
+      Lume''')
+
+print("Salutare\" Lume")
+print("Salutare Lume")
+print("Salutare Lume")
+print("Salutare Lume")
 print("Salutare Lume")
 
-print("Salutare Lume")
 
-print("Salutare Lume")
 
-print("Salutare Lume")
-print("Salutare Lume")
-print("Salutare Lume")
-print("Salutare Lume")
-print("Salutare Lume")
-print("Salutare Lume")
+x = input("Introdu ceva de la tastatura... ")
+
+print("Ai introdus: ", x)
