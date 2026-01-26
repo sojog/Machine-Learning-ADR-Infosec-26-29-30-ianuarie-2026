a25d5a7267c5b53bd518411441e2c0afe4697618
Silviu Ojog <sil.ojog@gmail.com>
2026-01-26 11:58:25 +0200

    Functionalitati Python

---
+++ 06.bucla_for.py
@@ -0,0 +1,15 @@
+## DIN ALTE LIMBAJE
+# for (int i = 0; i < 10; i++){
+#     print("i=",i)
+# }
+
+
+# Range este o colectie (secventa) de numere intregi
+for i in range(30, 33):
+    print("i=",i, type(i))
+
+print(range(10))
+
+
+for i in range(100, 120, 7):
+    print("i=",i, type(i))
